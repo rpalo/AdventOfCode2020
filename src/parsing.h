52e0bc21613fd9b53834b59fb8c2fdd2acb137fb
Ryan Palo <ryan@thepalos.com>
2020-12-03 10:55:18 -0800

    Document changes in day 3 to parsing file.

--- src/parsing.h
+++ src/parsing.h
@@ -8,11 +8,13 @@
 /// Assumes no newline at the end of the last line (so adds +1 more)
 size_t count_lines(FILE* fp);
 
+/// The height and width of a 2D grid.
 typedef struct {
   size_t width;
   size_t height;
 } GridSize;
 
+/// Takes in a file containing a 2D grid and returns its width/height.
 GridSize measure_grid(FILE* fp);
 
 #endif
